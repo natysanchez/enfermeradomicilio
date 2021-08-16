f24dab73269f40615d48892c3d8241819cdd8d94
natysanchez <88997194+natysanchez@users.noreply.github.com>
2021-08-15 22:15:45 -0500

    Add files via upload

---
+++ enviar2.php
@@ -0,0 +1,25 @@
+<?php
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$fecha = $_POST['fecha'];
+$servicio = $_POST['servicio'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'natysanchez2508@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, utf8_decode($mensaje,$fecha,$servicio,$telefono), $header);
+
+header("Location:index.html");
+?>
