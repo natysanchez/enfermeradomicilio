c3354d391f57a9f8176f2b04a36094f8b79bebe1
natysanchez <88997194+natysanchez@users.noreply.github.com>
2021-08-15 22:34:11 -0500

    Update enviar2.php

--- enviar2.php
+++ enviar2.php
@@ -19,7 +19,7 @@ $mensaje .= "Enviado el " . date('d/m/Y', time());
 $para = 'natysanchez2508@gmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
-mail($para, $asunto, utf8_decode($mensaje,$fecha,$servicio,$telefono), $header);
+mail($para, $asunto, utf8_decode($mensaje), $header);
 
 header("Location:index.html");
 ?>
